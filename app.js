0219be7bf89712bef24909c91030f77097dc7055
mohamed28666 <mohamed.belkahla19@gmail.com>
2021-04-20 04:25:18 +0200

    fist commit

--- app.js
+++ app.js
@@ -2,51 +2,116 @@ var express = require('express');
 var app = express();
 var path = require('path');
 const fs = require('fs');
-let student = { 
-    name: 'Mike',
-    age: 23, 
-    gender: 'Male',
-    department: 'English',
-    car: 'Honda' 
+const jwt = require('jsonwebtoken');
+const crpt = require('crypto');
+const dotenv = require('dotenv');
+var cors = require('cors');
+//jwt begin
+
+dotenv.config();
+process.env.TOKEN_SECRET;
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '324h' });
+}
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err);
+
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+
+    next();
+  });
+}
+
+//jwt end
+let student = {
+  name: 'Mike',
+  age: 23,
+  gender: 'Male',
+  department: 'English',
+  car: 'Honda'
 };
 let data = JSON.stringify(student, null, 2);
 var http = require('http').Server(app);
- var io = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 let rawdata = fs.readFileSync('relaystate.json');
 let R_state = JSON.parse(rawdata);
 console.log(R_state["relay"]);
 // read userfile
 let userraw = fs.readFileSync('users.json');
-let users=JSON.parse(userraw);
+let users = JSON.parse(userraw);
 
 app.set('port', process.env.PORT || 3333);
 
 app.use(express.static(__dirname + '/untitled_VR'));
-app.get('/troisd', function(req, res){
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');   
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend28.herokuapp.com');
-    
+
+//jwt get sample begin
+app.post('/api/login', function (req, res) {
+  console.log(req.body.password);
+  console.log(users.Admin.password);
+  console.log(req.body.email);
+  console.log(users.Admin.email);
   
-  res.sendFile(__dirname+'/untitled_VR/untitled_VR.65.html');  });
- //res.sendFile('/untitled_VR/untitled_VR.65.html',{ root: __dirname });  });
+  if ((users.Admin.email === req.body.email) && (users.Admin.password === req.body.password)) {
+    console.log("true")
+    const token = generateAccessToken({ username: req.body.email });
+    res.cookie('auth', token);
+    
+    res.redirect(303, 'http://localhost:3000/entered');
+  } else res.send("o93ed 3asba");
+
+});
 
 
- 
+//jwt get sample end
 
 
-app.get('/', function(req, res){
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend28.herokuapp.com');
- 
-res.type('text/plain');
-res.send(R_state);  });
 
-app.get('/:Relay', function(req, res){
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend28.herokuapp.com');
 
-res.type('text/plain');
-console.log(R_state[req.params.Relay]);
-res.send(R_state[req.params.Relay]); 
+app.get('/troisd', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');   
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+
+  res.sendFile(__dirname + '/untitled_VR/untitled_VR.65.html');
+});
+//res.sendFile('/untitled_VR/untitled_VR.65.html',{ root: __dirname });  });
+
+
+
+
+
+app.get('/', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  res.type('text/plain');
+  res.send(R_state);
+});
+
+app.get('/:Relay', function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+
+  res.type('text/plain');
+  console.log(R_state[req.params.Relay]);
+  res.send(R_state[req.params.Relay]);
 
 
 
@@ -54,59 +119,73 @@ res.send(R_state[req.params.Relay]);
 
 });
 
-app.get('/set/:Relay', function(req, res){
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend28.herokuapp.com');
+app.get('/set/:Relay', authenticateToken, (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
 
-R_state[req.params.Relay]="1";
-res.type('text/plain');
-console.log(R_state[req.params.Relay]);
-res.send("value update to"+R_state[req.params.Relay]); });
+  R_state[req.params.Relay] = "1";
+  res.type('text/plain');
+  console.log(R_state[req.params.Relay]);
+  res.send("value update to" + R_state[req.params.Relay]);
+});
 
-app.get('/reset/:Relay', function(req, res){
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend28.herokuapp.com');
- 
-R_state[req.params.Relay]="0";
-res.type('text/plain');
-console.log(R_state[req.params.Relay]);
-res.send("value updated to "+R_state[req.params.Relay]); });
-    
-  //http://localhost:3333/user/Admin/email  --> current email
-  //http://localhost:3333/user/Admin/password --> current password
-app.get('/user/:login/:email', function(req, res){
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend28.herokuapp.com');
+app.get('/reset/:Relay',authenticateToken, function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
 
- res.type('text/plain');
- res.send(users[req.params.login][req.params.email]);    
-  });    
-    
-    
- // custom 404 page    
- 
-    
- 
-    
+  R_state[req.params.Relay] = "0";
+  res.type('text/plain');
+  console.log(R_state[req.params.Relay]);
+  res.send("value updated to " + R_state[req.params.Relay]);
+});
 
+//http://localhost:3333/user/Admin/email  --> current email
+//http://localhost:3333/user/Admin/password --> current password
+app.get('/user/:login/:email', function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
 
-   
-
-
+  res.type('text/plain');
+  res.send(users[req.params.login][req.params.email]);
+});
 
 
-app.use(function(req, res){
-res.type('text/plain');
-res.status(404);
-res.send('404 - Not Found');
+// custom 404 page    
+
+
+
+
+
+
+
+
+
+
+
+app.use(function (req, res) {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
 });
 // custom 500 page
-app.use(function(err, req, res, next){
-console.error(err.stack);
-res.type('text/plain');
-res.status(500);
-res.send('500 - Server Error');
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
 });
-app.listen(app.get('port'), function(){
-console.log( 'Express started on https://frontend28.herokuapp.com:' +
-app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function () {
+  console.log('Express started on https://frontend28.herokuapp.com:' +
+    app.get('port') + '; press Ctrl-C to terminate.');
 });
 
 
@@ -115,8 +194,8 @@ app.get('port') + '; press Ctrl-C to terminate.' );
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'fron-endapp/my-app/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'fron-endapp/my-app/build', 'index.html'));
   });
 }
