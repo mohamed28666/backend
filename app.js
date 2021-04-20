bb6f850be0f1b90feeb9791f164016c3b96c8727
mohamed28666 <mohamed.belkahla19@gmail.com>
2021-04-21 01:01:56 +0200

    fist commit

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post('/api/login', function (req, res) {
     const token = generateAccessToken({ username: req.body.email });
     res.cookie('auth11', token,{
       expires: new Date(Date.now() + 8 * 3600000) // cookie will be removed after 8 hours
-    ,Domain: '.herokuapp.com'});
+    ,Domain: '.herokuapp.com',encode: String});
     //sessionstorage.setItem('auth', token);
     //console.log(localStorage.getItem('auth', token));
     // console.log("wsol houni")
