ad54defd64018eb7dd41177b825447a72ff543ef
mohamed28666 <mohamed.belkahla19@gmail.com>
2021-04-20 22:14:57 +0200

    fist commit

--- app.js
+++ app.js
@@ -212,3 +212,11 @@ if (process.env.NODE_ENV === 'production') {
 
 
 // ** MIDDLEWARE ** //
+//for deployement BEGIN//
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+//for deployement END//
