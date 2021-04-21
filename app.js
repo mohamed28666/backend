585c78347da37a29bf177003240b98b71dcee508
mohamed belkahla <mohamed.belkahla19@gmail.com>
2021-04-21 15:15:40 +0200

    first commit

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post('/api/login', function (req, res) {
     //}
     console.log(token);
 
-    res.redirect(303, 'http://localhost:3000/entered');
+    res.redirect(303, 'https://frontend288.herokuapp.com/entered/?token='+token);
 
   } else res.send("o93ed 3asba");
 
@@ -94,7 +94,7 @@ app.post('/api/login', function (req, res) {
 
 
 app.get('/troisd', function (req, res) {
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');   
+  // res.setHeader('Access-Control-Allow-Origin', 'https://frontend288.herokuapp.com');   
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
@@ -118,7 +118,7 @@ app.get('/', (req, res) => {
 //for deployement END//
 
 app.get('/SU', function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://frontend288.herokuapp.com');
 
   res.type('text/plain');
   res.send(R_state);
@@ -142,7 +142,7 @@ app.get('/:Relay', function (req, res) {
 });
 
 app.get('/set/:Relay', authenticateToken, (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://frontend288.herokuapp.com');
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
@@ -168,8 +168,8 @@ app.get('/reset/:Relay', authenticateToken, function (req, res) {
   res.send("value updated to " + R_state[req.params.Relay]);
 });
 
-//http://localhost:3333/user/Admin/email  --> current email
-//http://localhost:3333/user/Admin/password --> current password
+//https://backend28.herokuapp.com/user/Admin/email  --> current email
+//https://backend28.herokuapp.com/user/Admin/password --> current password
 app.get('/user/:login/:email', function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -207,7 +207,7 @@ app.use(function (err, req, res, next) {
   res.send('500 - Server Error');
 });
 app.listen(app.get('port'), function () {
-  console.log('Express started on http://localhost:3333:' +
+  console.log('Express started on https://backend28.herokuapp.com:' +
     app.get('port') + '; press Ctrl-C to terminate.');
 });
 
